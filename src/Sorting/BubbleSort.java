392a99401a40a46d2035ddb32110ae19899a8bc0
jaredfromguam <jlee671@yahoo.com>
2016-03-19 13:40:06 -0400

    Completed BubbleSort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -8,7 +8,22 @@ public class BubbleSort {
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
 		//TODO: S1 implements bubble sort
-		
-		return null;
+		int arrayLength = unsortedArray.size();
+		boolean swapped = true;
+		int j = 0;
+		int temp;
+		while(swapped){
+			swapped = false;
+			j++
+			for(int i = 0; i < arrayLength - j; i++){
+				if(unsortedArray[i] > unsortedArray[i + 1]){
+					temp = unsortedArray[i];
+					unsortedArray[i] = unsortedArray[i+1];
+					unsortedArray[i + 1] = temp;
+					swapped = true;
+				}
+			}
+		}
+		return unsortedArray;
 	}
 }
